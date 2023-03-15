56252c66765750f549fc79986cf90be726994a3d
Bert Heyman <bert.heyman@hotmail.com>
2023-03-15 14:40:40 +0100

    Improve user properties readability

--- src/main/kotlin/com/ehb/wc4relationschatroom/dto/CreateUserRequest.kt
+++ src/main/kotlin/com/ehb/wc4relationschatroom/dto/CreateUserRequest.kt
@@ -1,3 +1,7 @@
 package com.ehb.wc4relationschatroom.dto
 
-data class CreateUserRequest(var username: String, var email: String, var picture: String)
+data class CreateUserRequest(
+    var username: String,
+    var email: String,
+    var picture: String
+)
